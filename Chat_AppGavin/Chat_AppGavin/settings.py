6f823f82a91fcbe6e0a7fb12d86d3b133d2cae27
GavinAperano <audesandrine6@gmail.com>
2023-01-18 23:33:43 +0000

    set direct link for template

--- Chat_AppGavin/Chat_AppGavin/settings.py
+++ Chat_AppGavin/Chat_AppGavin/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,7 +61,7 @@ ROOT_URLCONF = 'Chat_AppGavin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, '../../../../app/chat/templates/chat')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
